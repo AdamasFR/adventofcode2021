18bdde877851dd38853cafc55b9e2781794cc31f
Anthony Dumas <anthony@dumas.cc>
2021-12-01 21:41:50 +0100

    Challenge 1.1 Completed

--- 1/challenge1_1/src/main.rs
+++ 1/challenge1_1/src/main.rs
@@ -1,26 +1,33 @@
 /**
  * Challenge 1_1
+ * * Obtained !
  * > https://adventofcode.com/2021/day/1
  */
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
-    let filename = "input/input_test.txt";
+    let filename = "input/input.txt";
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
 
-    let previous_line = 0;
-    let count = 0;
+    // assignation des variables
+    let mut previous_line = 0;
+    let mut count = 0;
 
     for line in reader.lines() {
-        println!("{}", line?);
-
-        if previous_line != 0 {
-            count = count + 1;
+        if let Ok(line_str) = line {
+            println!("{}", line_str);
+            let line_nbr = line_str.parse::<i32>().unwrap();
+            if previous_line != 0 && previous_line < line_nbr {
+                count = count + 1;
+            }
+            previous_line = line_nbr;
         }
-        previous_line = line.parse().unwrap();
+
     }
+
+    println!("Résultat final : {}", count);
 
     Ok(())
 }
