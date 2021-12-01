dbe9b2b5724fa49b233a674d1afd43f1131e1dbe
Anthony Dumas <anthony@dumas.cc>
2021-12-01 21:25:03 +0100

    Challenge 1_1

--- 1/challenge1_1/src/main.rs
+++ 1/challenge1_1/src/main.rs
@@ -2,7 +2,25 @@
  * Challenge 1_1
  * > https://adventofcode.com/2021/day/1
  */
-fn main() {
-    println!("Challenge 1_1");
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 
+fn main() -> io::Result<()> {
+    let filename = "input/input_test.txt";
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let previous_line = 0;
+    let count = 0;
+
+    for line in reader.lines() {
+        println!("{}", line?);
+
+        if previous_line != 0 {
+            count = count + 1;
+        }
+        previous_line = line.parse().unwrap();
+    }
+
+    Ok(())
 }
